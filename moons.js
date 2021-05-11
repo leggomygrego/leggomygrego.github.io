7ba065990a5faf8e7fca0d8708ee266108019d2b
Greg Allan <gja@fastmail.com>
2021-05-10 17:28:07 -0700

    syntax fix

--- moons.js
+++ moons.js
@@ -60,9 +60,7 @@ const dynamicFavicon = (favicon) => {
   link.rel = "shortcut icon";
   link.type = "image/svg+xml";
   link.href =
-    "data:image/svg+xml,
-    <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
-    <text y=%22.9em%22 font-size=%2290%22>" +
+    "data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22> <text y=%22.9em%22 font-size=%2290%22>" +
     favicon +
     "</text></svg>";
   document.head.appendChild(link);
